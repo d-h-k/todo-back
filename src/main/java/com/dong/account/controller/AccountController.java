d3271dc2307985b447a900630f07ebd2357346e5
d-h-k <ultraraptor@naver.com>
2022-07-27 23:52:41 +0900

    코드 포맷팅

--- src/main/java/com/dong/account/controller/AccountController.java
+++ src/main/java/com/dong/account/controller/AccountController.java
@@ -4,14 +4,12 @@ import com.dong.account.dto.AccountRequestDto;
 import com.dong.account.dto.AccountResponseDto;
 import com.dong.account.entity.Account;
 import com.dong.account.service.AccountService;
-import com.dong.common.ResponseWrapper;
 import com.dong.common.auth.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,7 +46,7 @@ public class AccountController {
     @GetMapping()
     public ResponseEntity<?> AccountList(Pageable pageable, @AuthUser Account account) {
         Page<Account> accounts = accountService.readPage(pageable);
-        Page<AccountResponseDto> responseDtos = pageResponse(accounts)
+        Page<AccountResponseDto> responseDtos = pageResponse(accounts);
 
 
         return listWrapOk(responseDtos).jsonResponse();
