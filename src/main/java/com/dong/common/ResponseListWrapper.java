1ae3e18b3a3f94156bf73159faa329c82135eb87
d-h-k <ultraraptor@naver.com>
2022-07-27 23:53:04 +0900

    리스트 래핑 클래스 구현

--- src/main/java/com/dong/common/ResponseListWrapper.java
+++ src/main/java/com/dong/common/ResponseListWrapper.java
@@ -1,4 +1,38 @@
 package com.dong.common;
 
-public class ResponseListWrapper {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseListWrapper<T extends Page<?>> {
+
+    private HttpStatus status;
+    private T contents;
+    private String message;
+    private Sort sort;
+
+    @SuppressWarnings("unchecked : type casting is safe")
+    public static <T extends Page<?>> ResponseListWrapper<T> listWrapOk(T contents) {
+        return (ResponseListWrapper<T>) ResponseListWrapper.builder()
+                .contents(contents)
+                .sort(contents.getSort())
+                .status(HttpStatus.OK)
+                .message(HttpStatus.OK.getReasonPhrase())
+                .build();
+    }
+
+    public ResponseEntity<?> jsonResponse() {
+        return ResponseEntity
+                .status(this.status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(contents);
+    }
 }
