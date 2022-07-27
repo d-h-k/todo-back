74b1736dcb49366b4c2846bcb12c8825a124dabd
d-h-k <ultraraptor@naver.com>
2022-07-27 19:55:25 +0900

    v1 컨트롤러 완성
    기본적인 crud 를 수행하는 컨트롤러 클래스 작성 완료

--- src/main/java/com/dong/todo/connect/TodoControllerV1.java
+++ src/main/java/com/dong/todo/connect/TodoControllerV1.java
@@ -1,29 +1,38 @@
 package com.dong.todo.connect;
 
+import com.dong.todo.domain.Todo;
 import com.dong.todo.dto.TodoDtoRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.dong.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
 public class TodoControllerV1 {
 
-    @GetMapping
+    private final TodoService todoService;
+
+    @PostMapping
     public Object create(@RequestBody TodoDtoRequest todoDtoRequest) {
+        return todoService.addTodo(todoDtoRequest.toEntity());
+    }
+
+    @GetMapping("/{id}")
+    public Object read(@PathVariable Long id) {
+        return todoService.read(id);
+    }
+
+    @GetMapping("/{id}")
+    public Object write(@PathVariable Long id,@RequestBody TodoDtoRequest todoDtoRequest) {
+        todoService.update(id, todoDtoRequest.toEntity());
         return null;
     }
 
-    @GetMapping
-    public Object read() {
-        return null;
-    }
-
-    @GetMapping
-    public Object write() {
-        return null;
-    }
-
-    @GetMapping
-    public Object delete() {
-        return null;
+    @PutMapping("/{id}")
+    public Object delete(@PathVariable Long id) {
+        return todoService.delete(id);
     }
 
 
