f8c17af67a9b32346709e12da652b3306fed837d
d-h-k <ultraraptor@naver.com>
2022-07-27 20:29:43 +0900

    엔티티에 업데이트 메서드 작성

--- src/main/java/com/dong/todo/domain/Todo.java
+++ src/main/java/com/dong/todo/domain/Todo.java
@@ -3,10 +3,7 @@ package com.dong.todo.domain;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Builder
 @NoArgsConstructor
@@ -14,16 +11,27 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @Entity
-@Table(name = "Todo")
+@Table(name = "todo")
 public class Todo {
 
     @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    private String id;
+    //@GeneratedValue(generator = "system-uuid")
+    //@GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    private String userId;
+    private Long userId;
     private String title;
+    private Long rank;
     private boolean done;
+    private boolean deleted;
 
+
+    public void update(Todo todo) {
+        this.userId = todo.userId;
+        this.title = todo.title;
+        this.rank = todo.rank;
+        this.done = todo.done;
+        this.deleted = todo.deleted;
+    }
 }
