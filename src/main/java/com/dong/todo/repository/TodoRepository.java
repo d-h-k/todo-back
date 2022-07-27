f78155704ba90fd167132e765377fcff35ad261e
d-h-k <ultraraptor@naver.com>
2022-07-27 20:30:07 +0900

    퍼시스턴트 레이어에 조회 메서드 추가

--- src/main/java/com/dong/todo/repository/TodoRepository.java
+++ src/main/java/com/dong/todo/repository/TodoRepository.java
@@ -3,8 +3,11 @@ package com.dong.todo.repository;
 import com.dong.todo.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 //Repository
 
+    Optional<Todo> findById(Long id);
 
 }
