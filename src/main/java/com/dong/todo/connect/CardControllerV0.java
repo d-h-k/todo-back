7cd132dafe00901bfa11d9a366ab44b49748a74c
dongdongKing <ultraraptor@naver.com>
2023-01-03 21:22:48 +0900

    feat : 도움말 추가

--- src/main/java/com/dong/todo/connect/CardControllerV0.java
+++ src/main/java/com/dong/todo/connect/CardControllerV0.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/card/v1")
-public class CardControllerV1 {
+@RequestMapping("/api/card/v0")
+public class CardControllerV0 {
+    // 일부러 멍청하고 이상하게 만든 코드임. v1 쓰셈..제발
 
     private final CardService cardService;
 
